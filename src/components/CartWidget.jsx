a425daf236c8114f2fd7f2093611c6ec83c7586a
carkotx <carkotx@gmail.com>
2023-11-08 11:50:58 -0300

    primer commit

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,11 @@
+import cart from '../assets/carrito-de-compras 2.png'
+
+export const CartWidget = () => {
+
+    return (
+    <>
+      <img src={cart} alt="Chango" width={30} />
+      <span>3</span>
+    </>
+    );
+};
